c1cdf9dd08a4e9fd1cccad52393f7758f4782ba3
jahpd <gcravista@gmail.com>
2015-11-13 12:35:15 -0200

    adicionado mais patchs

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ module.exports = {
 	    'Hi_1.pd',
 	    'Hi_2.pd',
 	    'Hi_3.pd',
+	    'Messages_1.pd',
+	    'Messages_2.pd',
+	    'Messages_3.pd',
+	    'Messages_4.pd',
+	    'Messages_5.pd',
 	]
     }
 };
